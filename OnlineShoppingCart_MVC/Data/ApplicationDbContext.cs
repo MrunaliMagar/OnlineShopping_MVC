6ae9f63c8ffc27802e2ec186e380c536ed659b2e
MrunaliMagae <mrunalipmagar@gmail.com>
2024-02-02 18:21:24 +0530

    testing

---
+++ OnlineShoppingCart_MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineShoppingCart_MVC.Models;
+
+namespace OnlineShoppingCart_MVC.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Product> Product { get; set; }
+    }
+}
---
+++ OnlineShoppingCart_MVC/Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShoppingCart_MVC.Models
+{
+    public class Product
+    {
+        [Key]
+        [Column("product_id")]
+        public int product_id { get; set; }
+
+        [Column("product_name")]
+        public string product_name { get; set;}
+
+        [Column("product_description")]
+        public string product_description { get; set;}
+
+        [Column("product_mrp")]
+        public string product_mrp { get; set;}
+
+        [Column("offer_price")]
+        public string product_offer_price { get; set;}
+
+        [Column("product_quantity")]
+        public int product_quantity { get; set;}
+
+        [Column("product_image")]
+        public string image_path { get; set;}
+
+        [ForeignKey("category_id")]
+        public Category? Category { get; set; }
+    }
+}
